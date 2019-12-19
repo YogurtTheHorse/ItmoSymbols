1666547c7d3b28fe0b9387f0c4cb74f219871736
YogurtTheHorse <yegorf1@gmail.com>
2019-12-19 21:56:26 +0300

    Add function for calculating simple formulas

--- SymbolComputations.Plots/Plots.cs
+++ SymbolComputations.Plots/Plots.cs
@@ -2,18 +2,29 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using MonoGame.Extended.Shapes;
+using SymbolComputations.Reducers;
+using SymbolComputations.Symbols;
+
 namespace SymbolComputations.Plots
 {
     public class Plots : Game
     {
+        private readonly Symbol _renderSymbol;
+        private readonly ReductionContext _context;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Vector3? _startPoint;
 
-        public Plots()
+        public Plots(Symbol renderSymbol, ReductionContext context)
         {
+            _renderSymbol = renderSymbol;
+            _context = context;
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            _startPoint = Formula(10) ?? Vector3.Zero;
         }
 
         protected override void LoadContent()
@@ -35,6 +46,24 @@ namespace SymbolComputations.Plots
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             base.Draw(gameTime);
+        }
+
+        private Vector3? Formula(float x)
+        {
+            Symbol symbol = _context.Reduce(_renderSymbol[x]);
+            var identifier = symbol as Identifier;
+            
+            return identifier switch
+            {
+                { Name: "Point" } => new Vector3(GetPointCoordinate(0), GetPointCoordinate(1), GetPointCoordinate(2)),
+                
+                _ => (Vector3?)null
+            };
+
+            float GetPointCoordinate(int i) => 
+                identifier.Tail.Count < i 
+                    ? 0 
+                    : (float) ((identifier.Tail[i] as Literal<decimal>)?.Value ?? 0);
         }
     }
 }
--- SymbolComputations.Plots/Program.cs
+++ SymbolComputations.Plots/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
+using SymbolComputations.Reducers;
+using SymbolComputations.Symbols;
+using static SymbolComputations.Std.Functions;
+using static SymbolComputations.Std.Math;
+using static SymbolComputations.Std.BuiltIns;
 
 namespace SymbolComputations.Plots
 {
     public static class Program
     {
         [STAThread]
-        static void Main()
+        private static void Main()
         {
-            using (var game = new Plots())
+            Identifier point = I("Point"), x = I("x");
+            
+            var ctx = new ReductionContext(
+                new BuiltInsReducer()
+            );
+
+            Symbol formula = Func(x)[
+                point[ReduceFirst[x]][ReduceFirst[x]][ReduceFirst[x]]
+            ];
+            
+            using (var game = new Plots(formula, ctx))
                 game.Run();
         }
     }
